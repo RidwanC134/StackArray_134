4b3c6c847e893c4e0c32d6ea08a4b5432ebb8186
RidwanC134 <ridwanhh10@gmail.com>
2023-06-15 15:38:53 +0700

    Melakukan decrement dan menampilkan element yang di pop

--- stackarray/stackarray.cpp
+++ stackarray/stackarray.cpp
@@ -33,7 +33,7 @@ public:
 			return; // 1. b
 		}
 
-		cout << "\nThe popped element is: " << stack_array[top] << endl;	// step 2
+		cout << "\nThe popped element is: " << stack_array[top] << endl;	// step 2 
 		top--; //step 3 decrement
 	}
 
